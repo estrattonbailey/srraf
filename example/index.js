9240a80374eb88caef21fe117b9edfe6ea5e38df
estrattonbailey <ericstrattonbailey@gmail.com>
2018-03-20 22:29:50 -0400

    v2

--- example/index.js
+++
@@ -1,9 +0,0 @@
-import srraf from '../package/index.js'
-
-window.srraf = {
-  // one: srraf.scroll.use(({ curr, prev }) => console.log('one')),
-  // two: srraf.scroll.use(({ curr, prev }) => console.log('two')),
-  // three: srraf.scroll.use(({ curr, prev }) => console.log('three')),
-}
-
-window.test = Object.create(srraf.scroll.use(({ curr, prev }) => console.log('test')))
---
+++ index.js
@@ -0,0 +1,42 @@
+let scroll = null
+let resize = null
+
+let { x: prevX, y: prevY } = pos()
+
+const fns = []
+
+function pos () {
+  if (typeof window === 'undefined') {}
+  return {
+    x: window.innerWidth,
+    y: window.pageYOffset
+  }
+}
+
+function handler (e) {
+  requestAnimationFrame(() => {
+    const { x, y } = pos()
+    for (let i = 0; i < fns.length; i++) {
+      fns[i]({ y, prevY, x, prevX }, e)
+    }
+    prevY = y
+    prevX = x
+  })
+}
+
+export default function srraf (fn) {
+  if (!scroll) window.addEventListener('scroll', handler)
+  if (!resize) window.addEventListener('resize', handler)
+
+  if (fns.indexOf(fn) < 0) fns.push(fn)
+
+  return {
+    destroy () {
+      fns.splice(fns.indexOf(fn), 1)
+    },
+    update () {
+      handler()
+      return this
+    }
+  }
+}
